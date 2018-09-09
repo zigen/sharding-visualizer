2b7b2076ca3835ced2556515e931165a4bd4535e
zigen <hrlclb@gmail.com>
2018-09-09 16:42:04 +0900

    show blocks as chain

--- client/index.js
+++ client/index.js
@@ -1,21 +1,90 @@
 import * as THREE from "three";
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 7;
+const subscribeCameraControl = elem => {
+  let clicked = false;
+  let prevMousePos = {};
+  let prevCameraPos = {};
+  elem.onmousedown = e => {
+    clicked = true;
+    prevMousePos = { x: e.clientX, y: e.clientY };
+    prevCameraPos = camera.position;
+  };
+  elem.onmousemove = e => {
+    if (clicked) {
+      camera.position.x =
+        (e.clientX - prevMousePos.x) * -0.001 + prevCameraPos.x;
+      camera.position.y =
+        (e.clientY - prevMousePos.y) * 0.001 + prevCameraPos.y;
+      camera.lookAt(cube.position);
+    }
+  };
+  elem.onmouseup = e => {
+    clicked = false;
+    prevMousePos = {};
+  };
+};
+
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.x = 3;
+light.position.z = 5;
+light.position.y = 5;
+scene.add(light);
+
+const blocks = [];
+const edges = [];
+const addLine = (b1, b2) => {
+  const material = new THREE.LineBasicMaterial({
+    color: 0xffffff,
+  });
+  const geometry = new THREE.Geometry();
+  geometry.vertices.push(b1.position, b2.position);
+  const line = new THREE.Line(geometry, material);
+  edges.push(line);
+  scene.add(line);
+};
+const addBlock = (x, y, z, parent) => {
+  const height = blocks.length;
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+  const block = new THREE.Mesh(geometry, material);
+  block.position.set(x, y, z);
+  blocks.push(block);
+  scene.add(block);
+  if (parent != null) {
+    addLine(block, parent);
+    return;
+  }
+  if (height > 0) {
+    addLine(blocks[height], blocks[height - 1]);
+  }
+};
+
+addBlock(0, 0, 0);
+addBlock(0, 2, 0);
+addBlock(0, 4, 0);
+addBlock(2, 4, 0, blocks[blocks.length - 2]);
+addBlock(2, 6, 0);
+
 const animate = () => {
-  requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render( scene, camera );
-}
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+subscribeCameraControl(renderer.domElement);
 animate();
+global.camera = camera;
+global.blocks = blocks;
+global.light = light;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./client/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
- module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env'],
-        }
-      }
-    }
-  ]
- },
-    devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 8080
+    port: 8080,
   },
-   plugins: [
-    new HtmlWebpackPlugin(),
-   ]
-}
+  plugins: [new HtmlWebpackPlugin()],
+};
