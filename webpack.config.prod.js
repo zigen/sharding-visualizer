da56de898503a661ef63b8195a5a7adaf1a1e8f0
zigen <hrlclb@gmail.com>
2018-10-01 22:47:56 +0900

    chore: add build script

---
+++ webpack.config.prod.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-flow"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      THREE: "three",
+    }),
+  ],
+};
