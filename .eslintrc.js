7fcfe9d1bc7572b8ddd0fdfbfcacf54760d32691
zigen <hrlclb@gmail.com>
2018-10-07 22:28:09 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,25 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  "extends": ["eslint:recommended", "plugin:flowtype/recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: ["eslint:recommended", "plugin:flowtype/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  "globals": {
-    "THREE": true
+  globals: {
+    THREE: true,
   },
-  "plugins": [
-    "react",
-    "flowtype"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
+  plugins: ["react", "flowtype"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
